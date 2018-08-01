5f182d1a016f71e2b1d0bcb4882c2cb6a9b9a133
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-08-02 08:54:38 +0900

    [wip] Write the code for authentication.

---
+++ Gribouillage.Mobile/Gribouillage/Models/FirebaseModel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using System.Linq;
+using System.Collections.ObjectModel;
+namespace Gribouillage.Models
+{
+  public class FirebaseModel : INotifyPropertyChanged
+  {
+    #region Property
+    /// <summary>
+    /// Email Address for Login.
+    /// </summary>
+    /// <value>The Email Address.</value>
+    public string Email
+    {
+      get => this._email;
+      set
+      {
+        if (this._email != value)
+        {
+          this._email = value;
+          this.OnPropertyChanged();
+        }
+      }
+    }
+
+    /// <summary>
+    /// Deault Email Address.
+    /// </summary>
+    private string _email = "huideyeren@gmail.com";
+
+    /// <summary>
+    /// Password for Login.
+    /// </summary>
+    /// <value>The password.</value>
+    public string Password
+    {
+      get => this._password;
+      set
+      {
+        if (this._password != value)
+        {
+          this._password = value;
+          this.OnPropertyChanged();
+        }
+      }
+    }
+
+    /// <summary>
+    /// Default Password.
+    /// </summary>
+    private string _password = "hogehoge";
+
+    /// <summary>
+    /// Auth Message.
+    /// </summary>
+    /// <value>Auth message.</value>
+    public string AuthMessage
+    {
+      get => this._authMessage;
+      set
+      {
+        if (this._authMessage != value)
+        {
+          this._authMessage = value;
+          this.OnPropertyChanged();
+        }
+      }
+    }
+
+      /// <summary>
+      /// default auth message.
+      /// </summary>
+    private string _authMessage;
+
+    #endregion
+
+    #region Variables
+    /// <summary>
+    /// Firebase認証へのリンク
+    /// </summary>
+    private FirebaseAuthLink _authLink;
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Sign in by Email Address and Password async.
+    /// </summary>
+    /// <returns>Task</returns>
+    public async Task SignInByEmailAndPasswordAsync()
+    {
+      try
+      {
+        var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebaseToken.ApiKey));
+
+        this._authLink = await auth.SignInWithEmailAndPasswordAsync(this.Email, this.Password);
+
+        this.AuthMessage = "サインインに成功しました。";
+      }
+      catch (FirebaseAuthException ex)
+      {
+        this.AuthMessage = "サインインできませんでした。エラーコード：" + ex.Reason;
+      }
+    }
+
+    public async Task SignUpByEmailAndPasswordAsync()
+    {
+      try
+      {
+        var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebaseToken.ApiKey));
+
+        this._authLink = await auth.CreateUserWithEmailAndPasswordAsync(this.Email, this.Password);
+
+        this.AuthMessage = "ユーザー作成に成功しました。";
+        }
+      catch (FirebaseAuthException ex)
+      {
+        this.AuthMessage = "ユーザー作成できませんでした。エラーコード：" + ex.Reason;
+      }
+    }
+    #endregion
+
+    #region Value
+
+    /// <summary>
+    /// Wrapper of string type.
+    /// </summary>
+    public class DatabaseData
+    {
+      public string Value { get; set; }
+    }
+
+    #endregion
+
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+  }
+}
---
+++ Gribouillage.Mobile/Gribouillage/Models/FirebaseToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gribouillage.Models
+{
+    static class FirebaseToken
+    {
+        /// <summary>
+        /// APIキー
+        /// </summary>
+        public static string ApiKey => "";
+
+        /// <summary>
+        /// 認証ドメイン
+        /// </summary>
+        public static string AuthDomain => "";
+
+        /// <summary>
+        /// データベースのURL
+        /// </summary>
+        public static string DatabaseUrl => "";
+
+        /// <summary>
+        /// ストレージバケット
+        /// </summary>
+        public static string StorageBucket => "";
+
+    }
+}
