5f5da27a38a7f39072e159da243c4e414af0f651
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-08-04 13:21:54 +0900

    Adding Binding.

--- Gribouillage.Mobile/Gribouillage/ViewModels/LoginPageViewModel.cs
+++ Gribouillage.Mobile/Gribouillage/ViewModels/LoginPageViewModel.cs
@@ -35,7 +35,7 @@ namespace Gribouillage.ViewModels
     #region Constructer
     public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
     {
-
+      this._firebaseModel.PropertyChanged += this.RaisePropertyChanged;
     }
     #endregion
 
