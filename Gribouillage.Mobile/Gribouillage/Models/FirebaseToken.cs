fb32e40cc301dd0d6ba44db027040bb94b681c59
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-08-06 08:12:14 +0900

    [wip] Setting API Key by dotenv.net

---
+++ Gribouillage.Mobile/Gribouillage/Models/FirebaseToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gribouillage.Models
+{
+  public static class FirebaseToken
+  {
+    /// <summary>
+    /// API Key
+    /// </summary>
+    public static string ApiKey => Environment.GetEnvironmentVariable("FirebaseApiKey");
+
+    /// <summary>
+    /// Auth Domain
+    /// </summary>
+    public static string AuthDomain => Environment.GetEnvironmentVariable("FirebaseAuthDomain");
+
+    /// <summary>
+    /// Database URL
+    /// </summary>
+    public static string DatabaseUrl => Environment.GetEnvironmentVariable("FirebaseDatabaseUrl");
+
+    /// <summary>
+    /// Storage Bucket
+    /// </summary>
+    public static string StorageBucket => Environment.GetEnvironmentVariable("FirebaseStorageBucket");
+
+    /// <summary>
+    /// Message Sender ID
+    /// </summary>
+    public static string MessagingSenderId => Environment.GetEnvironmentVariable("FirebaseMessagingSenderId");
+
+  }
+}
---
+++ Gribouillage.Mobile/Gribouillage/Models/VisualStudioAppCenterToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Gribouillage.Models
+{
+  public static class VisualStudioAppCenterToken
+  {
+    public static string AndroidKey => Environment.GetEnvironmentVariable("VsacAndroidKey");
+
+    public static string IOsKey => Environment.GetEnvironmentVariable("VsacIosKey");
+  }
+}
