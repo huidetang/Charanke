29d8d6ed44346764ad632ef3087c4e811c614c7b
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-09-21 08:55:59 +0900

    fix ios build

--- Charanke.Mobile/Charanke.iOS/appcenter-post-clone.sh
+++ Charanke.Mobile/Charanke.iOS/appcenter-post-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/Charanke.Mobile/Charanke.Android/google-services.json
+echo $GOOGLESERVICEINFO_PLIST | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/Charanke.Mobile/Charanke.iOS/GoogleService-Info.plist
