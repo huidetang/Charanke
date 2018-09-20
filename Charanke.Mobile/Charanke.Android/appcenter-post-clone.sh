1b98bdb96af61c20d0bcf2b75d3543dee6dbab45
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-09-21 08:23:36 +0900

    [wip]debugging in shellscript.

--- Charanke.Mobile/Charanke.Android/appcenter-post-clone.sh
+++ Charanke.Mobile/Charanke.Android/appcenter-post-clone.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+ls $APPCENTER_SOURCE_DIRECTORY
+
+ls $APPCENTER_SOURCE_DIRECTORY/Charanke.Mobile/Charanke.Android/
+
 echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/google-services.json
