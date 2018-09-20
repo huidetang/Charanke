846cbe0f2d9e51ef709b0b50a33a729e2e29e93f
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-09-21 08:26:58 +0900

    refs #31 fix android build.

--- Charanke.Mobile/Charanke.Android/appcenter-post-clone.sh
+++ Charanke.Mobile/Charanke.Android/appcenter-post-clone.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-ls $APPCENTER_SOURCE_DIRECTORY
-
-ls $APPCENTER_SOURCE_DIRECTORY/Charanke.Mobile/Charanke.Android/
-
-echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/google-services.json
+echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/Charanke.Mobile/Charanke.Android/google-services.json
